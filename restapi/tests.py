fdb6cb34069054f69f8d77d4cec9b576a07ebbdb
AdirO11 <adiromlet@gmail.com>
2021-03-30 22:34:44 +0300

    added first test and actions - removing comment

--- restapi/tests.py
+++ restapi/tests.py
@@ -4,11 +4,9 @@ import time
 
 
 def two_int_sum(a, b):
-    time.sleep(10)
     return a + b
 
 
 class TestSum(TestCase):
     def test_sum(self):
-        "testing sum"
         self.assertEqual(two_int_sum(1, 2), 3)
