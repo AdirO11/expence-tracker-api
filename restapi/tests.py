0db608397c3919ea4a6f8122c563854e34623665
AdirO11 <adiromlet@gmail.com>
2021-03-30 22:24:11 +0300

    added first test and actions - adding comment

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
 from unittest import TestCase
+import time
 
 
 def two_int_sum(a, b):
+    time.sleep(10)
     return a + b
 
 
 class TestSum(TestCase):
     def test_sum(self):
+        "testing sum"
         self.assertEqual(two_int_sum(1, 2), 3)
